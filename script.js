dcf7e341a4ad6a3b306221c9439308947fd6222b
Sameer-web-2006 <samasameer124@gmail.com>
2025-06-19 21:14:08 +0530

    Add Loader

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const emoji = document.getElementById("emoji");
 const name = document.getElementById("name");
 const btn = document.getElementById("btn");
 let emojis = [];
+const loader = document.getElementById("loader");
 const maxRetries = 3;
 
+const addLoad = () => {
+    loader.classList.add("loader");
+};
+const removeLoad = () => {
+    loader.classList.remove("loader");
+};
 // Load Emojis and store an object
 const loadEmojis = async () => {
+    addLoad();
     btn.disabled = true;
     for (let retries = 0; retries < maxRetries; retries++) {
         try {
@@ -17,12 +25,14 @@ const loadEmojis = async () => {
             emojis = response;
             btn.disabled = false;
             name.innerText = "Go... Get Emoji";
-
+            removeLoad();
             return;
             // console.log(emojis)
         } catch (e) {
             name.innerText = "Please check your internet and Try Again";
             btn.innerText = "Try Again";
+            btn.disabled = false;
+            removeLoad();
         }
     }
 };
@@ -30,20 +40,24 @@ const loadEmojis = async () => {
 // Print Emoji on the Screen
 
 const printEmojis = async () => {
+    addLoad();
     name.innerText = "Please Wait...";
     if (emojis.length === 0) {
         await loadEmojis();
     }
     const num = Math.floor(Math.random() * emojis.length);
     try {
+        removeLoad();
         emoji.innerText = emojis[num].character;
         name.innerText = emojis[num].unicodeName.toUpperCase();
+        btn.innerText = "Get another";
+        btn.disabled = false;
     } catch (error) {
+        removeLoad();
         name.innerText = "Please check your internet and Try Again";
         btn.innerText = "Try again";
     }
-    btn.innerText = "Get another";
-    btn.disabled = false;
+    
 };
 loadEmojis();
 btn.addEventListener("click", printEmojis);
